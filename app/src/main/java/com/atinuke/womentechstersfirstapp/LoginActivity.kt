204a46987c0f49bbc4c1342937ad0577d98168e1
Atinuke <aodeshola@yahoo.com>
2023-11-20 12:09:27 +0100

    Homework

--- app/src/main/java/com/atinuke/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/atinuke/womentechstersfirstapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.atinuke.womentechstersfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,5 +10,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        var backButton: Button = findViewById(R.id.backBtn)
+
+        backButton.setOnClickListener{
+            // Create an Intent to navigate to the LoginActivity
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+
+            // Start the LoginActivity
+            startActivity(intent)
+        }
     }
 }
